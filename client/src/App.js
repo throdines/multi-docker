1ef09328542bb6fad9748b6439ca6562f6e969e2
Thiago Pereira <thiago@Thiagos-MacBook-Pro.local>
2018-10-04 15:23:30 -0300

    bla

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to GABS</h1>
+            <h1 className="App-title">Welcome to Bla Bla Bla</h1>
             <Link to="/"> Home </Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
